e86a27f0ac503e56587dcadad2e69d36bb495e24
Henry AT <yiperu@gmail.com>
2020-05-09 18:54:05 -0400

    Using SwiftUI

--- Combine-quick look-publishers.playground/Contents.swift
+++ Combine-quick look-publishers.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Combine
+import SwiftUI
 
 let _ = Just(42).sink { (value) in
   
@@ -16,8 +17,8 @@ enum FutureError: Error {
 
 func performLongRunningNetworkTask(completion: (Bool, Error?) -> ()) {
   // long running network code here
-  // completion(true, nil)
-  completion(false, FutureError.TookTooLong)
+   completion(true, nil)
+//  completion(false, FutureError.TookTooLong)
 }
 
 let futurePublisher = Future<Bool, Error> { promise in
@@ -31,4 +32,27 @@ let futurePublisher = Future<Bool, Error> { promise in
   }
 }
 
+struct Employee: Identifiable {
+  var id = UUID()
+  var name: String
+  var title: String
+  
+}
+
+class WenderlichEmployee: ObservableObject {
+  
+  var employee: Employee
+  var employeeActive: Bool = true {
+    didSet {
+      willChange.send(self)
+    }
+  }
+  
+  var willChange = PassthroughSubject<WenderlichEmployee, Never>()
+  
+  init(employee: Employee) {
+    self.employee = employee
+    self.employeeActive = true
+  }
+}
 
